c5b0b71745906faf84f7377a0b1b8bfeeb9bb5d6
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2021-07-31 23:19:56 +0900

    implemented in-game recipe level.

--- www/src/components/ParameterEditor.tsx
+++ www/src/components/ParameterEditor.tsx
@@ -4,6 +4,106 @@ import { useLanguage } from "../hooks/useLanguage";
 import { CraftConfiguration, CraftParameter, ItemParameter, PlayerParameter, suggestedControls, suggestedCraftsmanships } from "../models/gamestate";
 import { translationProvider } from "../translation";
 
+const levelTable = {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "11": 11,
+    "12": 12,
+    "13": 13,
+    "14": 14,
+    "15": 15,
+    "16": 16,
+    "17": 17,
+    "18": 18,
+    "19": 19,
+    "20": 20,
+    "21": 21,
+    "22": 22,
+    "23": 23,
+    "24": 24,
+    "25": 25,
+    "26": 26,
+    "27": 27,
+    "28": 28,
+    "29": 29,
+    "30": 30,
+    "31": 31,
+    "32": 32,
+    "33": 33,
+    "34": 34,
+    "35": 35,
+    "36": 36,
+    "37": 37,
+    "38": 38,
+    "39": 39,
+    "40": 40,
+    "41": 41,
+    "42": 42,
+    "43": 43,
+    "44": 44,
+    "45": 45,
+    "46": 46,
+    "47": 47,
+    "48": 48,
+    "49": 49,
+    "50": 50,
+    "50★": 55,
+    "50★★": 70,
+    "50★★★": 90,
+    "50★★★★": 110,
+    "51": 120,
+    "52": 125,
+    "53": 130,
+    "54": 133,
+    "55": 136,
+    "56": 139,
+    "57": 142,
+    "58": 145,
+    "59": 148,
+    "60": 150,
+    "60★": 160,
+    "60★★": 180,
+    "60★★★": 210,
+    "60★★★★": 250,
+    "61": 260,
+    "62": 265,
+    "63": 270,
+    "64": 273,
+    "65": 276,
+    "66": 279,
+    "67": 282,
+    "68": 285,
+    "69": 288,
+    "70": 290,
+    "70★": 300,
+    "70★★": 320,
+    "70★★★": 350,
+    "70★★★★": 380,
+    "71": 390,
+    "72": 395,
+    "73": 400,
+    "74": 403,
+    "75": 406,
+    "76": 409,
+    "77": 412,
+    "78": 415,
+    "79": 418,
+    "80": 430,
+    "80★": 440,
+    "80★★": 450,
+    "80★★★": 480,
+    "80★★★★": 490,
+    "80★★★★★": 500,
+}
+
 interface Props {
     config: CraftConfiguration
     onChange: (CraftConfiguration) => void;
@@ -41,6 +141,8 @@ export function ParameterEditor(props: Props) {
             standard_control: newStandardControl
         })
     }
+    const recipeLevels = Object.keys(levelTable);
+    recipeLevels.sort((a, b) => levelTable[a] - levelTable[b]);
 
     return <Form>
         <Row>
@@ -64,7 +166,11 @@ export function ParameterEditor(props: Props) {
         <Row>
             <Form.Group as={Col}>
                 <Form.Label>{t("RecipeLevel")}</Form.Label>
-                <Form.Control value={config.params.item.internal_level} onChange={(e) => onItemParameterChange({...config.params.item, internal_level: parseInt(e.target.value)})}/>
+                <Form.Control as={"select"} onChange={(e) => onRecipeLevelChange(parseInt(e.target.value))}>
+                    {recipeLevels.map((recipeLevel) => {
+                        return <option key={recipeLevel} value={levelTable[recipeLevel]} selected={levelTable[recipeLevel] === config.params.item.internal_level}>{recipeLevel}</option>
+                    })}
+                </Form.Control>
             </Form.Group>
             <Form.Group as={Col}>
                 <Form.Label>{t("MaxDurability")}</Form.Label>
