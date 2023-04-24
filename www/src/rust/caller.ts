2a9813f40084afe6ef42654a8d53b0b11c26f19d
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2023-04-24 20:49:19 +0900

    wip

--- www/src/rust/caller.ts
+++ www/src/rust/caller.ts
@@ -1,24 +1,28 @@
 import { CraftAction, CraftParameter, CraftState } from "../models/gamestate";
 import { SupportedMethod } from "./const";
 
-const worker = new Worker("./worker.js");
-
-export function plan_macro(params: CraftParameter, initial_quality: number, longer: boolean): Promise<CraftAction[]> {
+function run(message: any[]): Promise<any> {
+    const worker = new Worker("./worker.js");
     return new Promise((resolve, reject) => {
         worker.onmessage = (event) => {
+            worker.terminate();
             resolve(event.data as CraftAction[]);
         }
-        worker.onerror = reject
-        worker.postMessage([SupportedMethod.PLAN_MACRO, params, initial_quality, longer]);
+        worker.onerror = (event) => {
+            worker.terminate();
+            reject(event);
+        }
+        worker.onmessageerror = () => {
+            worker.terminate();
+        }
+        worker.postMessage(message);
     })
 }
 
+export function plan_macro(params: CraftParameter, initial_quality: number, longer: boolean): Promise<CraftAction[]> {
+    return run([SupportedMethod.PLAN_MACRO, params, initial_quality, longer]);
+}
+
 export function search_best_move(params: CraftParameter, state: CraftState): Promise<CraftAction> {
-    return new Promise((resolve, reject) => {
-        worker.onmessage = (event) => {
-            resolve(event.data as CraftAction);
-        }
-        worker.onerror = reject
-        worker.postMessage([SupportedMethod.SEARCH_BEST_MOVE, params, state]);
-    })
+    return run([SupportedMethod.SEARCH_BEST_MOVE, params, state]);
 }
