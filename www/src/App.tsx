18627d7ec6f9ab9a6ddfc431f8f1eb6a5f0d36dd
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2025-04-06 21:39:20 +0900

    patch 7.2

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -8,28 +8,15 @@ import { Alert } from "react-bootstrap";
 
 export function App(props: {}) {
     const [language, setLanguage] = useLanguage();
-    const [showAlert, setShowAlert] = useState(true);
     const t = translationProvider(language)
     return (
         <>
             <div className={"m-5"}>
-                {showAlert ? <Alert variant="primary" onClose={() => setShowAlert(false)} dismissible>
-                    <p>
-                        {t("DawntrailReleased")}
-                    </p>
-                    <div>
-                        <Alert.Link href="https://github.com/amylase/ffxiv-crafter-rust/issues/">{t("ReportBug")}</Alert.Link>
-                    </div>
-                    <div>
-                        <Alert.Link href="https://www.shigure-hash.com/ffxiv-crafter-rust/endwalker/">{t("EndwalkerHere")}</Alert.Link>
-                        <span> {t("EndwalkerForKoreaChina")}</span>
-                    </div>
-                </Alert> : null}
                 <LanguageSelector/>
                 <CrafterGame/>
             </div>
             <div className={"ml-5 mb-2"}>
-                © 2024 <a href="https://github.com/amylase">amylase</a> (Shigure Hash@Typhon) and contributors. <a href="https://github.com/amylase/ffxiv-crafter-rust/issues">{t("ReportBug")}</a>
+                © 2025 <a href="https://github.com/amylase">amylase</a> (Shigure Hash@Typhon) and contributors. <a href="https://github.com/amylase/ffxiv-crafter-rust/issues">{t("ReportBug")}</a>
             </div>
         </>
     );
